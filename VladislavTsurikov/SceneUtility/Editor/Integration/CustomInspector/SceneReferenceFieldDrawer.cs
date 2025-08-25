e7784554a66e40a57c1ea35c56ae35de6fa08635
Vladislav Tsurikov <uladzislau78@gmail.com>
2025-08-25 18:58:01 +0300

    [Update] SceneUtility

---
+++ VladislavTsurikov/SceneUtility/Editor/Integration/CustomInspector/SceneReferenceFieldDrawer.cs
@@ -0,0 +1,40 @@
+#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using UnityEngine;
+using VladislavTsurikov.CustomInspector.Editor.IMGUI;
+using VladislavTsurikov.SceneUtility.Runtime;
+
+namespace VladislavTsurikov.SceneUtility.Editor.Integration.CustomInspector
+{
+    public class SceneReferenceFieldDrawer : IMGUIFieldDrawer
+    {
+        public override bool CanDraw(Type type)
+        {
+            if (type == typeof(SceneReference))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override object Draw(Rect rect, GUIContent label, Type fieldType, object value)
+        {
+            if (value is not SceneReference sceneReference)
+            {
+                return value;
+            }
+        
+            sceneReference.SceneAsset = (SceneAsset)EditorGUI.ObjectField(
+                rect, 
+                label, 
+                sceneReference.SceneAsset, 
+                typeof(SceneAsset), 
+                false);
+
+            return sceneReference;
+        }
+    }
+}
+#endif
---
+++ VladislavTsurikov/SceneUtility/Editor/Utility/BuildSceneUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+namespace VladislavTsurikov.SceneUtility.Editor.Utility
+{
+    public static class BuildSceneUtility
+    {
+        public static bool IsSceneInBuildSettings(string sceneName)
+        {
+            int sceneCount = SceneManager.sceneCountInBuildSettings;
+
+            for (int i = 0; i < sceneCount; i++)
+            {
+                string path = UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i);
+                string name = Path.GetFileNameWithoutExtension(path);
+
+                if (string.Equals(name, sceneName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
